283009bf362dd557213f7403c6022c24c1f3b00c
Lea Verou <me@leaverou.me>
2011-08-25 22:21:31 +0200

    Merged common functionality of CSS Snippets and CSS Controls into css-edit.js

---
+++ plugins/css-edit.js
@@ -0,0 +1,235 @@
+/**
+ * Dependency of CSS editing plugins like CSS Snippets or CSS Controls
+ * @author Lea Verou
+ * MIT License
+ */
+ 
+(function(){
+
+var guineaPig = document.createElement('div');
+ 
+var self = window.CSSEdit = {
+	setup: function(element) {
+		element.setAttribute('data-originalstyle', element.getAttribute('style'));
+		element.setAttribute('data-originalcssText', element.style.cssText);
+	},
+	
+	isCSSValid: function(code) {
+		var declarationCount = code.split(':').length - 1;
+			
+		guineaPig.removeAttribute('style');
+		guineaPig.setAttribute('style', code);
+		
+		return guineaPig.style.length >= declarationCount;
+	},
+	
+	prefixCSS: function(css) {
+		if (self.values.length) {
+			var regex = RegExp('\\b(' + self.values.join('|') + ')\\b', 'gi');
+			
+			css = css.replace(regex, self.prefix + "$1");
+		}
+		
+		if (self.properties.length) {
+			var regex = RegExp('\\b(' + self.properties.join('|') + '):', 'gi');
+			
+			css = css.replace(regex, self.prefix + "$1:");
+		}
+		
+		return css;
+	},
+	
+	getSubjects: function(element) {
+		var selector = element.getAttribute('data-subject'),
+			subjects;
+		
+		if(selector) {
+			subjects = self.util.toArray(document.querySelectorAll(selector)) || [];
+		}
+		else {
+			subjects = element.hasAttribute('data-subject')? [element] : [];
+		}
+		
+		if(/^(input|textarea)$/i.test(element.nodeName)) {
+			// If no subject specified, it will be the slide
+			if(!subjects.length) {
+				// Find containing slide
+				var slide = SlideShow.getSlide(element.parentNode);
+				
+				subjects = [slide? slide : element];
+			}
+		}
+		else {
+			subjects.unshift(element);
+		}
+		
+		return subjects;
+	},
+	
+	updateStyle: function(subjects, code) {
+		code = self.prefixCSS(code);
+		
+		if(self.isCSSValid(code)) {
+			guineaPig.setAttribute('style', code);
+			
+			var appliedCode = guineaPig.style.cssText,
+			    properties = appliedCode.match(/\b[a-z-]+(?=:)/gi), propRegex = [];
+			
+			for(var i=0; i<properties.length; i++) {
+				properties[i] = self.util.camelCase(properties[i]);
+			}
+			
+			for (var i=0; i<subjects.length; i++) {
+				var element = subjects[i],
+					prevStyle = subjects[i].getAttribute('style'),
+					style = prevStyle;
+				
+				if(prevStyle && prevStyle !== 'null') {	
+					for(var j=0; j<properties.length; j++) {
+						element.style[properties[i]] = null;
+					}
+					
+					element.setAttribute('style', element.getAttribute('data-originalstyle') + '; ' + code);
+				}
+				else {
+					element.setAttribute('style', code);
+				}
+			}
+			
+			return true;
+		}
+		else {
+			return false;
+		}
+	},
+	
+	util: {
+		camelCase: function(str) {
+			return str.replace(/-(.)/g, function($0, $1) { return $1.toUpperCase() })
+		},
+		
+		toArray: function(collection) {
+			return Array.prototype.slice.apply(collection);
+		}
+	}
+ };
+ 
+
+ 
+ self.prefix = (function() {
+	// Oldest prefixed properties that browsers still support
+	var props = {
+			'-moz-': 'MozOpacity',
+			'-webkit-': 'WebkitOpacity',
+			'-o-': 'OLink',
+			'-ms-': 'msFilter'
+		};
+	
+	// Determine which is the vendor prefix of the current browser
+	for (var prefix in props) {
+		var property = props[prefix];
+		
+		if (property in guineaPig.style) {
+			return prefix;
+		}
+	}
+	
+	return null;
+ })();
+ 
+ // Properties that *might* need prefixing
+ self.properties = [
+	'appearance',
+	'background-clip',
+	'background-origin',
+	'border-image',
+	'border-radius',
+	'box-decoration-break',
+	'box-shadow',
+	'box-sizing',
+	'column-count',
+	'column-rule',
+	'column-rule-width',
+	'column-rule-style',
+	'column-rule-color',
+	'column-span',
+	'tab-size',
+	'text-decoration-line',
+	'text-decoration-color',
+	'text-decoration-style',
+	'transform',
+	'transform-origin',
+	'transition',
+	'transition-duration',
+	'transition-property',
+	'transition-timing-function'
+ ];
+ 
+ // Values that *might* need prefixing and support tests
+ self.values = {
+	'repeating-linear-gradient': {
+		property: 'backgroundImage',
+		value: 'repeating-linear-gradient(white, black)'
+	},
+	'linear-gradient': {
+		property: 'backgroundImage',
+		value: 'linear-gradient(white, black)'
+	},
+	'repeating-radial-gradient': {
+		property: 'backgroundImage',
+		value: 'repeating-radial-gradient(white, black)'
+	},
+	'radial-gradient': {
+		property: 'backgroundImage',
+		value: 'radial-gradient(white, black)'
+	},
+	'calc': {
+		property: 'width',
+		value: 'calc(1px + 50%)'
+	},
+	'initial': {
+		property: 'color',
+		value: 'initial'
+	}
+ };
+ 
+ // Eliminate the ones that are either supported prefix-less or not at all
+ (function(properties, values, style) {
+	for (var i=0; i<properties.length; i++) {
+		var property = self.util.camelCase(properties[i]),
+			prefixedProperty = self.util.camelCase(self.prefix + properties[i]);
+ 
+		if (property  in style || !(prefixedProperty in style)) {
+			properties.splice(i--, 1);
+		}
+	}
+	
+	self.values = [];
+	
+	for (var val in values) {
+		// Try if prefix-less version is supported
+		var property = values[val].property,
+			value = values[val].value;
+		
+		style[property] = '';
+		style[property] = value;
+		
+		if (style[property]) {
+			continue;
+		}
+		
+		// Now try with a prefix
+		style[property] = '';
+		style[property] = self.prefix + value;
+		
+		if (!style[property]) {
+			continue;
+		}
+		
+		// If we're here, it is supported, but with a prefix
+		self.values.push(val);
+	}
+ })(self.properties, self.values, guineaPig.style);
+
+ 
+ })()
--- plugins/css-snippets.js
+++ plugins/css-snippets.js
@@ -1,5 +1,6 @@
 /**
  * Script to add prefixes to standard CSS3 in textareas or style attributes
+ * Requires css-edit.js
  * @author Lea Verou
  * MIT License
  */
@@ -7,11 +8,12 @@
 (function() {
 
 var self = window.CSSSnippet = function(element) {
-	var me = this,
-	    selector = element.getAttribute('data-subject');
+	var me = this;
 	
 	// this holds the elements the CSS is gonna be applied to
-	this.subjects = selector? toArray(document.querySelectorAll(selector)) || [] : (element.hasAttribute('data-subject')? [element] : []);
+	this.subjects = CSSEdit.getSubjects(element);
+	
+	CSSEdit.setup(element);
 
 	// Test if its text field first
 	if(/^(input|textarea)$/i.test(element.nodeName)) {
@@ -19,20 +21,6 @@ var self = window.CSSSnippet = function(element) {
 		
 		// Turn spellchecking off
 		this.textField.spellcheck = false;
-		
-		// If no subject specified, it will be the slide
-		if(!this.subjects.length) {
-			// Find containing slide
-			this.slide = SlideShow.getSlide(this.textField.parentNode);
-			
-			if(this.slide) {		
-				this.subjects = [this.slide];
-			}
-			else {
-				// If no slide present, it will be the text field itself
-				this.subjects = [this.textField];
-			}
-		}
 		
 		this.textField.addEventListener('keyup', function() {
 			me.update();
@@ -45,12 +33,6 @@ var self = window.CSSSnippet = function(element) {
 		this.update();
 	}
 	else {
-		// Not a text field, add current element as a subject
-		this.subjects.unshift(element);
-		
-		// Keep original style, just in case
-		element.setAttribute('data-originalstyle', element.getAttribute('style'));
-		
 		// Update style, only once
 		this.updateStyle();
 	}
@@ -58,21 +40,12 @@ var self = window.CSSSnippet = function(element) {
 
 self.prototype = {
 	updateStyle: function() {
-		var supportedStyle = self.prefixCSS(this.getCSS());
+		var supportedStyle = CSSEdit.prefixCSS(this.getCSS());
 		
-		// If it's a text field, test if style is valid first to avoid applying 
-		// random stuff in between valid keystrokes
-		if(this.textField) {
-			if (!self.isCSSValid(supportedStyle)) {
-				this.textField.classList && this.textField.classList.add('error');
-				return;
-			}
-			
-			this.textField.classList && this.textField.classList.remove('error');
-		}
+		var valid = CSSEdit.updateStyle(this.subjects, this.getCSS());
 		
-		for (var i=0; i<this.subjects.length; i++) {
-			this.subjects[i].setAttribute('style', supportedStyle);
+		if(this.textField && this.textField.classList) {
+			this.textField.classList[valid? 'remove' : 'add']('error');
 		}
 	},
 	
@@ -93,166 +66,5 @@ self.prototype = {
 		this.adjustHeight();
 	}
 };
-
-/*****************************
- * Static properties 
- *****************************/
-self.guineaPig = document.createElement('div');
-
-self.prefix = (function() {
-	var element = self.guineaPig,
-		// Oldest prefixed properties that browsers still support
-		props = {
-			'-moz-': 'MozOpacity',
-			'-webkit-': 'WebkitOpacity',
-			'-o-': 'OLink',
-			'-ms-': 'msFilter'
-		};
-	
-	// Determine which is the vendor prefix of the current browser
-	for (var prefix in props) {
-		var property = props[prefix];
-		
-		if (property in element.style) {
-			return prefix;
-		}
-	}
-	
-	return null;
-})();
-
-// Properties that *might* need prefixing
-self.properties = [
-	'appearance',
-	'background-clip',
-	'background-origin',
-	'border-image',
-	'border-radius',
-	'box-decoration-break',
-	'box-shadow',
-	'box-sizing',
-	'column-count',
-	'column-rule',
-	'column-rule-width',
-	'column-rule-style',
-	'column-rule-color',
-	'column-span',
-	'tab-size',
-	'text-decoration-line',
-	'text-decoration-color',
-	'text-decoration-style',
-	'transform',
-	'transform-origin',
-	'transition',
-	'transition-duration',
-	'transition-property',
-	'transition-timing-function'
-];
-
-// Values that *might* need prefixing and support tests
-self.values = {
-	'repeating-linear-gradient': {
-		property: 'backgroundImage',
-		value: 'repeating-linear-gradient(white, black)'
-	},
-	'linear-gradient': {
-		property: 'backgroundImage',
-		value: 'linear-gradient(white, black)'
-	},
-	'repeating-radial-gradient': {
-		property: 'backgroundImage',
-		value: 'repeating-radial-gradient(white, black)'
-	},
-	'radial-gradient': {
-		property: 'backgroundImage',
-		value: 'radial-gradient(white, black)'
-	},
-	'calc': {
-		property: 'width',
-		value: 'calc(1px + 50%)'
-	},
-	'initial': {
-		property: 'color',
-		value: 'initial'
-	}
-};
-
-// Eliminate the ones that are either supported prefix-less or not at all
-(function(properties, values, style) {
-	for (var i=0; i<properties.length; i++) {
-		var property = camelCase(properties[i]),
-			prefixedProperty = camelCase(self.prefix + properties[i]);
-
-		if (property  in style || !(prefixedProperty in style)) {
-			properties.splice(i--, 1);
-		}
-	}
-	
-	self.values = [];
-	
-	for (var val in values) {
-		// Try if prefix-less version is supported
-		var property = values[val].property,
-			value = values[val].value;
-		
-		style[property] = '';
-		style[property] = value;
-		
-		if (style[property]) {
-			continue;
-		}
-		
-		// Now try with a prefix
-		style[property] = '';
-		style[property] = self.prefix + value;
-		
-		if (!style[property]) {
-			continue;
-		}
-		
-		// If we're here, it is supported, but with a prefix
-		self.values.push(val);
-	}
-})(self.properties, self.values, self.guineaPig.style);
-
-/**********************************************
- * Static methods 
- **********************************************/
-self.prefixCSS = function(css) {
-	if (self.values.length) {
-		var regex = RegExp('\\b(' + self.values.join('|') + ')\\b', 'gi');
-		
-		css = css.replace(regex, self.prefix + "$1");
-	}
-	
-	if (self.properties.length) {
-		var regex = RegExp('\\b(' + self.properties.join('|') + '):', 'gi');
-		
-		css = css.replace(regex, self.prefix + "$1:");
-	}
-	
-	return css;
-};
-
-self.isCSSValid = function(code) {
-	var guineaPig = self.guineaPig,
-		declarationCount = code.split(':').length - 1;
-		
-	guineaPig.removeAttribute('style');
-	guineaPig.setAttribute('style', code);
-	
-	return guineaPig.style.length >= declarationCount;
-};
-
-/**********************************************
- * Private helpers
- **********************************************/
-function camelCase(str) {
-	return str.replace(/-(.)/g, function($0, $1) { return $1.toUpperCase() })
-}
-
-function toArray(collection) {
-	return Array.prototype.slice.apply(collection);
-}
 
 })();
