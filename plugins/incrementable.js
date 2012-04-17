e69ac027fd46c5f51b06b4a5f750e139efb834f5
Lea Verou <lea@verou.me>
2012-04-16 21:26:11 -0700

    Updated incrementable

--- plugins/incrementable.js
+++ plugins/incrementable.js
@@ -1,17 +1,17 @@
 /**
  * Script for making multiple numbers in a textfield incrementable/decrementable (like Firebug's CSS values)
  * @author Lea Verou
- * @version 1.0
+ * @version 1.1
  */
+ 
+(function(){
 
 /**
  * Constructor
  * @param textField {HTMLElement} An input or textarea element
- * @param modifiers {Object} An object with params ctrlKey, altKey and/or shiftKey. The key combination must have a sum of >= 3.
- * 							For example, {altKey: 1, ctrlKey: 3, shiftKey: 2} means that either Ctrl has to be pressed with the arrows, or alt+shift, or alt+ctrl, or all three.
- *							The default is 0, which means no modifiers needed.
+ * @param multiplier {Function} A function that accepts the event object and returns the multiplier or 0 for nothing to happen.
  */
-function Incrementable(textField, modifiers, units) {
+var _ = window.Incrementable = function(textField, multiplier, units) {
 	var me = this;
 
 	this.textField = textField;
@@ -19,12 +19,13 @@ function Incrementable(textField, modifiers, units) {
 	this.step = +textField.getAttribute('step') || 
 				+textField.getAttribute('data-step') || 1;
 
-	modifiers = modifiers || {};
-	this.modifiers = {
-		ctrlKey: modifiers.ctrlKey || 0,
-		altKey: modifiers.altKey || 0,
-		shiftKey: modifiers.shiftKey || 0
-	};
+	this.multiplier = multiplier || function(evt) {
+		if(evt.shiftKey) { return 10; }
+		
+		if(evt.ctrlKey) { return .1; }
+		
+		return 1;
+	}
 
 	if(units) {
 		this.units = units;
@@ -33,18 +34,23 @@ function Incrementable(textField, modifiers, units) {
 	this.changed = false;
 
 	this.textField.addEventListener('keydown', function(evt) {
-		if(me.checkModifiers(evt) && (evt.keyCode == 38 || evt.keyCode == 40)) {
+		var multiplier = me.multiplier(evt);
+		
+		if(multiplier && (evt.keyCode == 38 || evt.keyCode == 40)) {
 			me.changed = false;
 			
 			// Up or down arrow pressed, check if there's something
 			// increment/decrement-able where the caret is
 			var caret = this.selectionStart, text = this.value,
-				regex = new RegExp('^([\\s\\S]{0,' + caret + '}[^-0-9\\.])(-?[0-9]*(?:\\.?[0-9]+)(?:' + me.units + '))\\b', 'i');
-
-			this.value = this.value.replace(regex, function($0, $1, $2) {
+				regex = new RegExp('^([\\s\\S]{0,' + caret + '}[^-0-9\\.])(-?[0-9]*(?:\\.?[0-9]+)(?:' + me.units + '))\\b', 'i'),
+				property = 'value' in this? 'value' : 'textContent';
+			
+			this[property] = this[property].replace(regex, function($0, $1, $2) {
 				if($1.length <= caret && $1.length + $2.length >= caret) {
 					me.changed = true;
-					return $1 + me.stepValue($2, evt.keyCode == 40, evt.shiftKey);
+					var stepValue = me.stepValue($2, evt.keyCode == 40, multiplier);
+					caret = caret + (stepValue.length - $2.length);
+					return $1 + stepValue;
 				}
 				else {
 					return $1 + $2;
@@ -52,7 +58,8 @@ function Incrementable(textField, modifiers, units) {
 			});
 
 			if(me.changed) {
-				this.selectionEnd = this.selectionStart = caret;
+				this.setSelectionRange(caret, caret);
+				
 				evt.preventDefault();
 				evt.stopPropagation();
 			}
@@ -60,35 +67,51 @@ function Incrementable(textField, modifiers, units) {
 	}, false);
 
 	this.textField.addEventListener('keypress', function(evt) {
-		if(me.changed && me.checkModifiers(evt) 
-			&& (evt.keyCode == 38 || evt.keyCode == 40))
+		if(me.changed && (evt.keyCode == 38 || evt.keyCode == 40))
 			evt.preventDefault();
 			evt.stopPropagation();
 			me.changed = false;
 	}, false);
 }
 
-Incrementable.prototype = {
-	checkModifiers: function(evt) {
-		var m = this.modifiers;
-
-		return m.ctrlKey * evt.ctrlKey + m.altKey * evt.altKey + m.shiftKey * evt.shiftKey >= 3
-				|| (m.ctrlKey + m.altKey + m.shiftKey == 0);
-	},
-	
+_.prototype = {
 	/**
 	 * Gets a <length> and increments or decrements it
 	 */
-	stepValue: function(length, decrement, byChunk) {
-		var val = parseFloat(length) + (decrement? -1 : 1) * (byChunk? 10 : 1) * this.step;
+	stepValue: function(length, decrement, multiplier) {
+		var val = parseFloat(length),
+			offset = (decrement? -1 : 1) * (multiplier || 1) * this.step,
+			valPrecision = precision(val),
+			offsetPrecision = precision(offset);
 		
 		// Prevent rounding errors
-		if(this.step % 1) {
-			val = (parseFloat(val.toPrecision(12)));
-		}
+		var newVal = (parseFloat((val + offset).toPrecision(
+			Math.max(valPrecision.integer, offsetPrecision.integer) +
+			Math.max(valPrecision.decimals, offsetPrecision.decimals)
+		)));
 		
-		return val + length.replace(/^-|[0-9]+|\./g, '');
+		return newVal + length.replace(/^-|[0-9]+|\./g, '');
 	},
 
-	units: '|%|deg|px|r?em|ex|ch|in|cm|mm|pt|pc|vm|vw|vh|gd|m?s'
+	units: '|%|deg|px|r?em|ex|ch|in|cm|mm|pt|pc|vmin|vw|vh|gd|m?s'
 };
+
+function precision(number) {
+	number = (number + '').replace(/^0+/, '');
+	
+	var dot = number.indexOf('.');
+	
+	if (dot === -1) {
+		return {
+			integer: number.length,
+			decimals: 0
+		};
+	}
+	
+	return {
+		integer: dot,
+		decimals: number.length - 1 - dot
+	};
+}
+
+})();
