61f41852bfed8467e1960d82bdedfb67d87f826c
Lea Verou <lea@verou.me>
2018-10-08 13:44:31 +0100

    [resolution] Apply resolution on individual slides!

--- plugins/resolution/plugin.js
+++ plugins/resolution/plugin.js
@@ -1,21 +1,56 @@
 {
 
-let forceResolution;
+// let forceResolution;
+//
+// if (forceResolution = $("[data-resolution]")) {
+// 	let [width, height] = forceResolution.dataset.resolution.split(/\s+/);
+//
+// 	forceResolution.style.setProperty("--vw", width);
+// 	forceResolution.style.setProperty("--vh", height);
+//
+// 	var adjustZoom = () => {
+// 		let [wratio, hratio] = [innerWidth / width, innerHeight / height];
+//
+// 		document.documentElement.style.zoom = Math.min(wratio, hratio) * 100 + "%";
+// 	}
+//
+// 	adjustZoom();
+// 	addEventListener("resize", adjustZoom, {passive: true});
+// }
 
-if (forceResolution = $("[data-resolution]")) {
-	let [width, height] = forceResolution.dataset.resolution.split(/\s+/);
+$$("[data-resolution]").forEach(element => {
+	let [width, height] = element.closest("[data-resolution]").dataset.resolution.split(/\s+/);
+	element.style.setProperty("--vw", width);
+	element.style.setProperty("--vh", height);
+});
 
-	forceResolution.style.setProperty("--vw", width);
-	forceResolution.style.setProperty("--vh", height);
+let width, height;
 
-	var adjustZoom = () => {
+var adjustZoom = () => {
+	let zoom = "";
+console.count()
+	if (width && height) {
 		let [wratio, hratio] = [innerWidth / width, innerHeight / height];
 
-		document.documentElement.style.zoom = Math.min(wratio, hratio) * 100 + "%";
+		zoom = Math.min(wratio, hratio) * 100 + "%";
 	}
 
-	adjustZoom();
-	addEventListener("resize", adjustZoom, {passive: true});
+	document.documentElement.style.zoom = zoom;
 }
+
+Inspire.hooks.add("slidechange", env => {
+	if (Inspire.currentSlide.closest("[data-resolution]")) {
+		let cs = getComputedStyle(Inspire.currentSlide);
+		[width, height] = [cs.getPropertyValue("--vw"), cs.getPropertyValue("--vh")];
+		adjustZoom();
+		addEventListener("resize", adjustZoom, {passive: true});
+	}
+	else if (env.prevSlide.closest("[data-resolution]")) {
+		// Cleanup
+		[width, height] = ["", ""];
+		adjustZoom();
+		removeEventListener("resize", adjustZoom, {passive: true});
+	}
+});
 
 }
