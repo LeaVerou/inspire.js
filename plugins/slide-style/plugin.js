b09e2121e107bfaccbf6b8826a8dfc7215625882
Lea Verou <lea@verou.me>
2018-10-12 17:11:04 +0100

    [slide-style] Bliss.$ to $$

--- plugins/slide-style/plugin.js
+++ plugins/slide-style/plugin.js
@@ -1,4 +1,4 @@
 Inspire.hooks.add("slidechange", env => {
-	Bliss.$(".slide:not(:target) style[data-slide]").forEach(style => style.disabled = true);
-	Bliss.$("style[data-slide]", env.slide).forEach(style => style.disabled = false);
+	$$(".slide:not(:target) style[data-slide]").forEach(style => style.disabled = true);
+	$$("style[data-slide]", env.slide).forEach(style => style.disabled = false);
 });
