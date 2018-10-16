6e40b96f677f7835259f6c5a5075b66b83ebcb84
Lea Verou <lea@verou.me>
2018-10-17 00:58:15 +0100

    [docs] Allow additional classes

--- plugins/docs/plugin.js
+++ plugins/docs/plugin.js
@@ -3,28 +3,24 @@ $$(Inspire.pluginTest.docs).forEach(code => {
 	var text = code.dataset.mdn? "" : code.textContent;
 	var path;
 
-	switch (code.className) {
-		case "function":
-			code.textContent += "()"; // pass-through
-		case "property":
-		case "css":
-			path = "CSS";
-			break;
-		case "element":
-			path = "HTML/Element";
-			code.textContent = "<" + text + ">";
-			break;
-		case "svg element":
-			path = "SVG/Element";
-			code.textContent = "<" + text + ">";
-			break;
-		case "attribute":
-			var category = code.dataset.category || "Global_attributes";
-			path = `API/${category}`;
-			break;
-		default:
-			var mdn = code.closest("[data-mdn]");
-			path = mdn? mdn.dataset.mdn : "";
+	if (code.matches(".element")) {
+		path = code.matches(".svg")? "SVG/Element" : "HTML/Element"
+		code.textContent = "<" + text + ">";
+	}
+	else if (code.matches(".function, .property, .css")) {
+		path = "CSS";
+	}
+	else if (code.matches(".attribute")) {
+		var category = code.dataset.category || "Global_attributes";
+		path = `API/${category}`;
+	}
+	else {
+		var mdn = code.closest("[data-mdn]");
+		path = mdn? mdn.dataset.mdn : "";
+	}
+
+	if (code.matches(".function")) {
+		code.textContent += "()";	
 	}
 
 	$.create("a", {
