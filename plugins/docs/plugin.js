61a6d4e96f0c774e36e99bb2238fb8ab08ba5672
Lea Verou <lea@verou.me>
2020-03-28 23:49:58 -0400

    [docs] Make styling easier, .docs-icons to counter .no-docs-icons

--- plugins/docs/plugin.js
+++ plugins/docs/plugin.js
@@ -1,6 +1,6 @@
 // Links to documentation
 $$(Inspire.plugins.docs).forEach(code => {
-	var text = code.dataset.mdn? "" : code.textContent;
+	var text = code.dataset.mdn && !/\/$/.test(code.dataset.mdn)? "" : code.textContent;
 	var path;
 	var svg = code.matches(".svg");
 
@@ -30,6 +30,7 @@ $$(Inspire.plugins.docs).forEach(code => {
 	}
 
 	$.create("a", {
+		className: "docs-link",
 		href: `https://developer.mozilla.org/en-US/docs/Web/${path}/${text}`,
 		around: code,
 		target: "_blank"
