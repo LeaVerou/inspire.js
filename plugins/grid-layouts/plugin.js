fd0bf1cf2e9168363ed4437d76ffa55f5056e75d
Lea Verou <lea@verou.me>
2020-06-29 21:51:08 -0400

    [grid-layouts] Improve code with a little js

---
+++ plugins/grid-layouts/plugin.js
@@ -0,0 +1,15 @@
+$$('[class*="heading-"]').forEach(el => {
+	let [, m, n] = el.className.match(/\bheading-(\d)x(\d)/) || [];
+
+	if (m && n) {
+		el.classList.add("heading-MxN");
+
+		if (!el.style.getPropertyValue("--columns")) {
+			el.style.setProperty("--columns", m);
+		}
+
+		if (!el.style.getPropertyValue("--rows")) {
+			el.style.setProperty("--rows", n);
+		}
+	}
+});
