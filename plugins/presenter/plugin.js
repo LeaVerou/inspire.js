b08cef80f014af5c6bc743bd0a4a689dd561184f
Lea Verou <lea@verou.me>
2021-05-24 14:37:15 +0300

    [presenter] Bidirectional sync between presenter and projector views

--- plugins/presenter/plugin.js
+++ plugins/presenter/plugin.js
@@ -1,15 +1,16 @@
 Inspire.hooks.add({
 	"init-end": me => {
 		if (window.name === "projector" && window.opener && opener.Inspire) {
+			// Projector window was reloaded
 			document.body.classList.add("projector");
-			Inspire.presenter = opener.Inspire;
-			Inspire.presenter.projector = this;
+			Inspire.presenter = opener;
+			Inspire.presenter.Inspire.projector = this;
 		}
 	},
 	"keyup": env => {
-		// Ctrl+P : Presenter view
+		// Ctrl+P : Open Presenter view
 		if (env.letter === "P") {
-			// Open new window for attendee view
+			// Open new window for projector view
 			Inspire.projector = open(location, "projector");
 
 			// Get the focus back
@@ -18,20 +19,49 @@ Inspire.hooks.add({
 			// Switch this one to presenter view
 			document.body.classList.add("presenter", "show-next");
 
-			// Are there <details class="notes"> elements? Open them all
+			// Are there <details class="notes"> elements in the current slide? Open them
+			$$("details.notes", Inspire.currentSlide).forEach(d => d.open = true);
 		}
 	},
 	"slidechange": env => {
-		if (Inspire.projector) {
-			var pInspire = Inspire.projector.Inspire;
-			pInspire.goto(env.which);
+		let otherWindow = Inspire.projector || Inspire.presenter;
 
+		if (Inspire.isActive && otherWindow) {
+			// Sync slide navigation
+			otherWindow.Inspire.goto(env.which);
+		}
+
+		if (Inspire.projector) {
 			$$("details.notes", Inspire.currentSlide).forEach(d => d.open = true);
 		}
 	},
 	"gotoitem-end": env => {
-		if (Inspire.projector) {
-			Inspire.projector.Inspire.gotoItem(env.which);
+		let otherWindow = Inspire.projector || Inspire.presenter;
+
+		if (Inspire.isActive && otherWindow) {
+			// Sync slide item navigation
+			otherWindow.Inspire.gotoItem(env.which);
 		}
 	}
+});
+
+
+// Track whether presenter or projector is the active window
+addEventListener("focus", _ => {
+	Inspire.isActive = true;
+
+	// If this window is focused, no other can be
+	if (Inspire.projector) {
+		Inspire.projector.Inspire.isActive = false;
+	}
+	else if (Inspire.presenter) {
+		Inspire.presenter.Inspire.isActive = false;
+	}
+});
+
+addEventListener("blur", _ => {
+	Inspire.isActive = false;
+
+	// If this window is not focused,
+	// we cannot make assumptions about which one is.
 });
