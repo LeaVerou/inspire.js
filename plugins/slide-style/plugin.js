15279be31a22a8017fc79126301416e29d957629
Lea Verou <lea@verou.me>
2021-06-01 18:05:14 +0300

    [slide-style] Ability to add media="not all" to avoid too many reflows

--- plugins/slide-style/plugin.js
+++ plugins/slide-style/plugin.js
@@ -1,4 +1,11 @@
 Inspire.hooks.add("slidechange", env => {
 	$$(".slide:not(:target) style[data-slide]").forEach(style => style.disabled = true);
-	$$("style[data-slide]", env.slide).forEach(style => style.disabled = false);
+	$$("style[data-slide]", env.slide).forEach(style => {
+		if (style.media === "not all") {
+			// This is used to prevent the style applying before the plugin is loaded
+			style.removeAttribute("media");
+		}
+
+		style.disabled = false;
+	});
 });
