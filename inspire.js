60afa6b5798c4624f2f43d04206044161d3e702a
Lea Verou <lea@verou.me>
2021-06-08 18:22:44 +0300

    [balance-lines] Fix loading

--- inspire.js
+++ inspire.js
@@ -50,7 +50,7 @@ const _ = self.Inspire = {
 		"mavo": "[mv-app]",
 		"visible-keys": "[data-visible-keys]",
 		"grid-layouts": "[class*='heading-']",
-		"balance-lines": ".balance-lines",
+		"balance-lines": ".balance-lines, [data-balance-elements]",
 		"details-notes": "details.notes",
 		"markdown": "[data-markdown-elements]"
 	},
